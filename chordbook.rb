a4bee2c7fdd02a2b3a70274f89638e53010cb757
Kirill Bobykin <qelphybox@gmail.com>
2025-11-03 19:35:26 +0400

    add blank message handling

--- chordbook.rb
+++ chordbook.rb
@@ -1,1 +1,11 @@
-puts "Hello, World!"
+require 'telegram/bot'
+
+def handle_message(bot, message)
+  # TODO: Implement message handling
+end
+
+Telegram::Bot::Client.run(ENV['TG_API_TOKEN']) do |bot|
+  bot.listen do |message|
+    handle_message(bot, message)
+  end
+end
